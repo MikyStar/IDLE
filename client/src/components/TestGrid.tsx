bcb111d8468a1ac4e09de7def8ff59cd67144443
AUBENAS Mike <mike.aubenas@gmail.com>
2019-12-13 22:48:24 +0100

    Improve tests on react grid layout + import linters settings

--- client/src/components/TestGrid.tsx
+++ client/src/components/TestGrid.tsx
@@ -13,7 +13,7 @@ export default class TestGrid extends React.Component
 			{ i: 'b', x: 1, y: 5, w: 3, h: 2, static : true },
 			{ i: 'c', x: 4, y: 0, w: 1, h: 3, static : true },
 			{ i: 'd', x: 4, y: 0, w: 6, h: 3, static : true },
-			{ i: 'e', x: 0, y: 2, w: 2, h: 2, static : true }
+			{ i: 'e', x: 5, y: 10, w: 7, h: 2, static : true }
 		];
 
 		return	(
@@ -24,15 +24,47 @@ export default class TestGrid extends React.Component
 						cols={ 12 }
 						rowHeight={30}
 						width={1000}
-						compactType={"horizontal"}
 						isDraggable={ false }
 					>
 						<div key="a" style={{ backgroundColor : 'red' }}>a</div>
 						<div key="b" style={{ backgroundColor : 'blue' }}>b</div>
 						<div key="c" style={{ backgroundColor : 'green' }}>c</div>
-						<div key="d" style={{ backgroundColor : 'pink' }}>d</div>
-						<div key="e" style={{ backgroundColor : 'cornflowerblue' }}>e</div>
+						<div key="d" style={{ backgroundColor : 'pink' }}>
+							<p>1</p>
+							<p>2</p>
+							<p>3</p>
+							<p>4</p>
+						</div>
+						<div key="e" style={{ backgroundColor : 'cornflowerblue' }}>--<EmbededGrid /></div>
 					</GridLayout>
 				);
 	}
+}
+
+//////////////////////////////////////////////////////////////////////
+
+const EmbededGrid = () =>
+{
+	const layout =
+		[
+			{ i: 'f', x: 0, y: 0, w: 1, h: 1, static : true },
+			{ i: 'g', x: 1, y: 5, w: 2, h: 2, static : true },
+			{ i: 'h', x: 2, y: 0, w: 5, h: 2, static : true },
+		];
+
+		return	(
+					<GridLayout
+						style={{ backgroundColor : 'white' }}
+						className="layout"
+						layout={layout}
+						cols={ 12 }
+						rowHeight={30}
+						width={1000}
+						isDraggable={ false }
+					>
+						<div key="f" style={{ backgroundColor : 'grey' }}>f</div>
+						<div key="g" style={{ backgroundColor : 'darkblue' }}>g</div>
+						<div key="h" style={{ backgroundColor : 'darkgreen' }}>h</div>
+					</GridLayout>
+				);
 }
