3505b793623f4cddf644418a52ce59d5f62bd896
AUBENAS Mike <mike.aubenas@gmail.com>
2019-12-15 03:48:45 +0100

    Add util CSS border style + finish building layout

--- client/src/components/containers/Building.tsx
+++ client/src/components/containers/Building.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import GridLayout from 'react-grid-layout';
 
-import '../../styles/css/Building.css';
+import '../../styles/css/utils/Borders.css';
 import star from '../../assets/icons/star.svg';
 import people from '../../assets/icons/people.svg';
 import hammer from '../../assets/icons/hammer.svg';
 import bolt from '../../assets/icons/bolt.svg';
+import arrowUp from '../../assets/icons/arrow-up.svg';
 
 //////////////////////////////////////////////////////////////////////
 
@@ -38,33 +39,42 @@ const Building : React.FC<BuildingProps> = ( { type, material, level, workers, p
 	return	(
 				<>
 					<GridLayout
-						className={ 'layout border' }
+						className={ 'layout border-normal' }
 						style={{ width : `${ WIDTH }px` }}
 						layout=	{[
-									{ i: 'type', x: 0, y: 0, w: 2, h: 1, static : true },
-									{ i: 'material', x: 4, y: 0, w: 2, h: 1, static : true },
+									{ i: 'type', x: 2, y: 0, w: 2, h: 1, static : true },
+									{ i: 'material', x: 6, y: 0, w: 2, h: 1, static : true },
 									{ i: 'levelIcon', x: 0, y: 2, w: 2, h: 1, static : true },
 									{ i: 'level', x: 4, y: 2, w: 2, h: 1, static : true },
 									{ i: 'workersIcon', x: 0, y: 4, w: 2, h: 1, static : true },
 									{ i: 'workers', x: 4, y: 4, w: 2, h: 1, static : true },
 									{ i: 'productionIcon', x: 0, y: 6, w: 2, h: 1, static : true },
 									{ i: 'productionRate', x: 4, y: 6, w: 2, h: 1, static : true },
-									{ i: 'upgradePrice', x: 8, y: 3.5, w: 2, h: 1, static : true },
+									{ i: 'upgrade', x: 8, y: 3, w: 3, h: 2, static : true },
 								]}
 						rowHeight={ 20 }
-						cols={ 9 }
+						cols={ 12 }
 						width={ WIDTH }
 						autoSize
 					>
 						<div key='type' style={{ fontWeight : 'bolder', textAlign : 'center' }}>{ type }</div>
 						<div key='material' style={{ fontStyle : 'italic' }}>{ material }</div>
-						<img key='levelIcon' src={ star } />
+
+						<img key='levelIcon' src={ star } alt='Level' title='Level' />
 						<div key='level'>{ level } / 5</div>
-						<img key='workersIcon' src={ people } />
+
+						<img key='workersIcon' src={ people } alt='Workers' title='Workers' />
 						<div key='workers'>{ workers } / 3</div>
-						<img key='productionIcon' src={ productionIconSVG } />
+
+						<img key='productionIcon' src={ productionIconSVG } alt={ `${ type }` } title={ `${ type }` } />
 						<div key='productionRate'>{ productionRate } / s</div>
-						<div key='upgradePrice' style={{ fontWeight : 'bold' }}>{ upgradePrice }</div>
+
+						<div key='upgrade' className='border-normal' style={{ fontWeight : 'bolder', fontSize : '20px', padding : '5px', position : 'relative' }}>
+							<div style={{ alignItems : 'center', display : 'flex', float : 'left', position : 'absolute', top : '50%', left : '50%', transform : 'translate(-50%, -50%)' }}>
+								<img src={ arrowUp } alt='Upgrade' title='Upgrade' height='30px' style={{ padding : '5px' }}/>
+								<div style={{ padding : '5px' }}>{ upgradePrice }</div>
+							</div>
+						</div>
 					</GridLayout>
 				</>
 			);
