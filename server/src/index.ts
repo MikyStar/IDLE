6634c79631d0eabbf02a9870d10071fd48f80047
AUBENAS Mike <mike.aubenas@gmail.com>
2020-01-02 18:24:16 +0100

    Server can run and access .env

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,52 @@
 #!/usr/bin/env node
 import { install as installSourceMapSupport } from 'source-map-support';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+
+////////////////////////////////////////////////////////////////////////////////
+
+const DEFAULT_PORT = 4000;
+dotenv.config();
 
 ////////////////////////////////////////////////////////////////////////////////
 
 export const main = async ( argv: string[] ) : Promise<void> =>
 {
-	console.log( "Hello, world!" );
+
+	const app = express();
+
+	////////////////////////////////////////////////////////////////////////////
+
+	const onError = ( err: any ) =>
+	{
+		console.log( err );
+		process.exit( 1 );
+	}
+
+	const allowJSONBodyHandling = () => app.use( bodyParser.json() );
+
+	const enableCORS = () => app.use( cors() );
+
+	const launchServer = () =>
+	{
+		app.listen( process.env.port || DEFAULT_PORT, () =>
+		{
+			console.log( `NodeJS server running in ${ process.env.NODE_ENV } mode.` );
+		});
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+
+	allowJSONBodyHandling();
+	enableCORS();
+
+	launchServer();
+
+	process.on( "uncaughtException", onError );
+	process.on( "unhandledRejection", onError );
 }
-
-////////////////////////////////////////////////////////////////////////////////
-
-const onError = ( err: any ) =>
-{
-	console.log( err );
-	process.exit( 1 );
-}
-
-process.on("uncaughtException", onError);
-process.on("unhandledRejection", onError);
 
 ///////////////////////////////////////////////////////////////////////////////
 
