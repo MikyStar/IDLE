cdf793426b5bc0ced252239cc6836398e4269523
AUBENAS Mike <mike.aubenas@gmail.com>
2020-01-30 18:56:15 +0100

    Improve default value management

--- server/src/true-model/User.ts
+++ server/src/true-model/User.ts
@@ -1,34 +1,39 @@
 import { Entity, Column, PrimaryGeneratedColumn, BaseEntity, ObjectIdColumn } from "typeorm";
+import moment from 'moment';
+
+////////////////////////////////////////////////////////////////////////////
+
+const TIMESTAMP_FORMAT = "DD/MM/YYYY HH:mm:ss";
 
 ////////////////////////////////////////////////////////////////////////////
 
 @Entity()
 export class User extends BaseEntity
 {
-    @ObjectIdColumn()
+    @ObjectIdColumn({ primary : true })
     id : string;
     
-    @Column('string')
+    @Column('string', { unique : true,  })
     email : string;
 
     @Column('string')
     passwordHash : string;
 
-    @Column('number', { default : 5000 })
-    money : number;
+    @Column('number')
+    money : number = 5000;
 
-    @Column('number', { default : 0 })
-    production : number;
+    @Column('number')
+    production : number = 0;
 
-    @Column('datetime', { default : Date.now })
-    lastUpdate : Date;
-
-    @Column('array')
-    staff : [string];
+    @Column('datetime')
+    lastUpdate : Date = moment().format( TIMESTAMP_FORMAT );
 
     @Column('array')
-    buildings : [string];
+    staff : string[] = [];
 
-    @Column('number', { default : 6 })
-    slotsAvailable : number;
+    @Column('array')
+    buildings : string[] = [];
+
+    @Column('number')
+    slotsAvailable : number = 6;
 }
